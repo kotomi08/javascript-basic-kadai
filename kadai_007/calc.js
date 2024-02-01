f478d1e81cf1555767dfacb1e6981fdac23804a8
近藤　琴美 <kotomi@kondoukotominoMacBook-Air.local>
2024-01-31 16:32:49 -0800

    課題007を追加

---
+++ kadai_007/calc.js
@@ -0,0 +1,25 @@
+//numにランダムな整数を代入する
+let num = Math.floor(Math.random() * 31)+1;
+
+//numの値を出力する（確認用）
+console.log(num);
+
+//変数(num)が3と5の倍数の場合
+if(num % 3 === 0 && num % 5 === 0){
+    console.log('3と5の倍数です')
+}
+
+//変数(num)が3の倍数の場合
+if(num % 3 === 0){
+    console.log('3の倍数です')
+}
+
+//変数(num)が5の倍数の場合
+else if(num % 5 === 0){
+    console.log('5の倍数です')
+}
+
+//それ以外の場合
+else{
+    console.log(num);
+}
